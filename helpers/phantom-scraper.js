8da7f1d83c12d4f5c171a0d1a4c2218d6eb5d65e
Ralph Menguito <publiclass1@gmail.com>
2017-12-28 21:01:48 +0800

    add error handler for load balancer

--- helpers/phantom-scraper.js
+++ helpers/phantom-scraper.js
@@ -1,7 +1,7 @@
 const exec = require('child_process').exec
 const script = require.resolve(__dirname + '/../scripts/browser.js')
 const cheerio = require('cheerio')
-
+const fs = require('fs')
 
 module.exports = (url) => {
   return new Promise((resolve, reject) => {
@@ -16,7 +16,27 @@ module.exports = (url) => {
         return reject(err)
       }
       const $ = cheerio.load(stdout)
-      resolve($)
+      const fileErrorPath = __dirname + '/../amz.err'
+      //check if amazon response 503
+      if ($('title').text().trim() === 'Sorry! Something went wrong!') {
+        fs.exists(fileErrorPath, function (_exists) {
+          if (_exists) return resolve($)
+
+          fs.writeFile(fileErrorPath, '1', function (err) {
+            resolve($)
+          })
+        });
+
+      } else {
+        fs.exists(fileErrorPath, function (_exists) {
+          if (!_exists) return resolve($)
+
+          fs.unlink(fileErrorPath, function () {
+            resolve($)
+          })
+        })
+      }
+
     })
   })
 }
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.json({
     name: 'amz crawler'
+  })
+});
+
+router.get('/health-check', function (req, res) {
+  fs.exists(__dirname + '/../amz.err', function (err) {
+    if (err) {
+      res.send(500, 'Something went wrong!')
+    } else {
+      res.json({
+        health: 'ok'
+      })
+    }
   })
 });
 
